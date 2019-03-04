1c9e0577c675a0cfa8d8946546d0a8c4a3c64f44
Theresa <tli2@cca.edu>
2019-03-04 11:35:40 -0800

    Remove semi-colons from Grub class

--- src/Grub.js
+++ src/Grub.js
@@ -1,8 +1,8 @@
 class Grub {
   constructor () {
-    this.age = 0;
-    this.color = 'pink';
-    this.food = 'jelly';
+    this.age = 0
+    this.color = 'pink'
+    this.food = 'jelly'
   }
 
   eat () {
